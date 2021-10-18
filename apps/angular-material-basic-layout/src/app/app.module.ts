433a164dcb856fde0b3c70b9e67a10abd9671be6
Saptarshi <saptarshi27@gmail.com>
2021-10-18 18:43:54 +0530

    Removed Material library

--- apps/angular-material-basic-layout/src/app/app.module.ts
+++ apps/angular-material-basic-layout/src/app/app.module.ts
@@ -2,7 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { MaterialModule } from '@devon4ts-samples/material';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppRoutingModule } from './app-routing.module';
 
@@ -15,7 +21,12 @@ import { DataComponent } from './pages/data/data.component';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MaterialModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule,
     AppRoutingModule,
   ],
   providers: [],

81cbf448494dac839730c46b397e458079c14411
Saptarshi <saptarshi27@gmail.com>
2021-10-25 18:03:53 +0530

    Created sample angular lilbrary

---
+++ libs/angular-library/src/lib/angular-library.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AngularLibraryService {
+
+  constructor() { }
+
+  firstLetterToUpper(word: string): string {
+    let firstLetter = word.substring(0, 1);
+    const restWord = word.substring(1, word.length);
+
+    firstLetter = firstLetter.toUpperCase();
+    const wordFirstUpper = firstLetter + restWord;
+    return wordFirstUpper;
+  }
+}

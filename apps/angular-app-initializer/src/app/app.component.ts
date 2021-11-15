1177655bdbe6874cb9a7521db3b793d9c0c6fc4c
Akanksha <akanksha.a.agarwal@capgemini.com>
2021-11-15 17:41:17 +0530

    created app-initializer sample

---
+++ apps/angular-app-initializer/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ConfigService } from './core/config/config.service';
+
+@Component({
+  selector: 'devon4ts-samples-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'angular-app-initializer';
+  constructor(public configService: ConfigService) { }
+}
+

e08f8e52a7e2a1f43a65023ea0e838ad63cde522
Akanksha <akanksha.a.agarwal@capgemini.com>
2021-11-17 13:09:17 +0530

    generated ionic-basic-pwa sample

---
+++ apps/ionic-basic-pwa/src/app/home/home.page.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Plugins, CameraResultType } from '@capacitor/core';
+
+const { camera, toast } = Plugins;
+
+@Component({
+  selector: 'devon4ts-samples-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+  image: SafeResourceUrl;
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.image = this.sanitizer.bypassSecurityTrustResourceUrl(
+      '../../assets/capgemini-logo.png'
+    );
+  }
+
+  async takePicture() {
+    try {
+      const image = await camera.getPhoto({
+        quality: 90,
+        allowEditing: true,
+        resultType: CameraResultType.Uri,
+      });
+
+      // Change last picture shown
+      this.image = this.sanitizer.bypassSecurityTrustResourceUrl(image.webPath);
+    } catch (e) {
+      this.show('Closing camera');
+    }
+  }
+
+  async show(message: string) {
+    await toast.show({
+      text: message,
+    });
+  }
+}
